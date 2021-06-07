83fefd26189a55d8ac2262937d7372bdda11da38
Tai Zhou <21986946+Tai-Zhou@users.noreply.github.com>
2021-06-07 21:22:23 +0800

    Add comment

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,20 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as superagent from 'superagent';
 
-let config: vscode.WorkspaceConfiguration;
-var userHash: string;
-var showImages: boolean;
-var showRelated: boolean;
-var autoRefresh: number;
-var keyWords: string[];
-var keysLength: number[];
-var blockWords: string[];
-var period: number;
-const periodDic = ['48', 'weekhot', 'weekcomment', 'month'];
-var showThumbs: boolean;
-var lastNewsId: number = -1;
+let config: vscode.WorkspaceConfiguration; // 所有设置信息
+var userHash: string; // 通行证 Cookie
+var showImages: boolean; // 显示图片
+var showRelated: boolean; // 显示相关文章
+var autoRefresh: number; // “最新”刷新间隔
+var keyWords: string[]; // 关键词列表
+var keysLength: number[]; // 关键词长度
+var blockWords: string[]; // 屏蔽词列表
+var period: number; // “热榜”榜单，仅在启动时从设置中读取
+const periodDic = ['48', 'weekhot', 'weekcomment', 'month']; // “热榜”榜单字典
+var showThumbs: boolean; // “热评”显示点赞数
+var lastNewsId: number = -1; // “最新”最后阅读标记，用于显示上次阅读位置
 
-function refreshConfig() {
+function refreshConfig() { // 刷新设置，仅在手动刷新时运行
 	config = vscode.workspace.getConfiguration('ith2ome');
 	userHash = <string>config.get('account');
 	showImages = <boolean>config.get('showImages');
@@ -30,14 +28,14 @@ function refreshConfig() {
 	showThumbs = <boolean>config.get('showThumbs');
 }
 
-function show(title: string): boolean {
+function show(title: string): boolean { // 返回是否显示该条新闻
 	for (var i in blockWords)
 		if (title.includes(blockWords[i]))
 			return false;
 	return true;
 }
 
-function highlight(title: string): [number, number][] {
+function highlight(title: string): [number, number][] { // 返回该条新闻关键词位置
 	var highlights: [number, number][] = [];
 	var loc: number;
 	for (var i in keyWords)
@@ -46,15 +44,15 @@ function highlight(title: string): [number, number][] {
 	return highlights;
 }
 
-function linkCheck(url: string): vscode.Uri {
+function linkCheck(url: string): vscode.Uri { // 检查链接是否以 https:// 开始
 	return vscode.Uri.parse(url.substr(0, 5) == 'https' ? url : 'https://www.ithome.com' + url);
 }
 
-class ith2omeItem extends vscode.TreeItem {
+class ith2omeItem extends vscode.TreeItem { // 在 TreeItem 基础上增加 shareInfo 用于复制链接
 	shareInfo?: string;
 }
 
-class ith2omeShowContent implements vscode.Command {
+class ith2omeShowContent implements vscode.Command { // 在 VSCode 中查看的命令，用于“最新”、“热榜”与“热评”
 	title = '查看内容';
 	command = 'ith2ome.showContent';
 	arguments: string[];
@@ -63,23 +61,23 @@ class ith2omeShowContent implements vscode.Command {
 	}
 }
 
-class contentProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
-	private update = new vscode.EventEmitter<vscode.TreeItem | void>();
+class contentProvider implements vscode.TreeDataProvider<vscode.TreeItem> { // 为 View 提供内容
+	private update = new vscode.EventEmitter<vscode.TreeItem | void>(); // 用于触发刷新
 	readonly onDidChangeTreeData = this.update.event;
-	private list: ith2omeItem[] = [];
-	private mode: number;
-	private refreshTimer: NodeJS.Timeout | undefined;
+	private list: ith2omeItem[] = []; // 项目列表
+	private mode: number; // 工作模式，0 为“通行证”，1 为“最新”，2 为“热榜”，3 为“热评”
+	private refreshTimer: NodeJS.Timeout | undefined; // 自动刷新计时器
 
 	constructor(_mode: number) {
 		this.mode = _mode;
 		this.refresh();
 	}
 	refresh() {
-		if (this.refreshTimer)
-			clearTimeout(this.refreshTimer);
-		this.list = [];
-		if (this.mode == 0) {
-			if (userHash == '') {
+		if (this.refreshTimer) // 若为手动刷新
+			clearTimeout(this.refreshTimer); // 清除下一次自动刷新计时器
+		this.list = []; // 清除项目列表
+		if (this.mode == 0) { // “通行证”
+			if (userHash == '') { // Cookie 为空
 				this.list = [{
 					label: '使用 Cookie 登录通行证',
 					iconPath: new vscode.ThemeIcon('log-in'),
@@ -90,7 +88,7 @@ class contentProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
 			}
 			else {
 				superagent.get(`https://my.ruanmei.com/api/User/Get?userHash=${userHash}&extra`).end((err, res) => {
-					if (!res.body.ok) {
+					if (!res.body.ok) { // 失败
 						userHash = '';
 						config.update('account', '', true);
 						vscode.window.showErrorMessage('登录失败，请检查 Cookie。');
@@ -119,10 +117,10 @@ class contentProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
 						this.update.fire();
 					});
 				});
-				this.refreshTimer = setTimeout(() => { this.refresh(); }, 3600000);
+				this.refreshTimer = setTimeout(() => { this.refresh(); }, 3600000); // 设置自动刷新时间
 			}
 		}
-		else if (this.mode == 1) {
+		else if (this.mode == 1) { // “最新”
 			superagent.get('https://api.ithome.com/json/newslist/news').end((err, res) => {
 				var latest: number = 0;
 				let topList = res.body.toplist;
@@ -173,9 +171,9 @@ class contentProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
 				this.update.fire();
 			});
 			if (autoRefresh > 0)
-				this.refreshTimer = setTimeout(() => { this.refresh(); }, autoRefresh * 1000);
+				this.refreshTimer = setTimeout(() => { this.refresh(); }, autoRefresh * 1000); // 设置自动刷新时间
 		}
-		else if (this.mode == 2) {
+		else if (this.mode == 2) { // “热榜”
 			superagent.get('https://api.ithome.com/json/newslist/rank').end((err, res) => {
 				let rankList = res.body['channel' + periodDic[period] + 'rank'];
 				for (var i in rankList) {
@@ -193,9 +191,9 @@ class contentProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
 				}
 				this.update.fire();
 			});
-			this.refreshTimer = setTimeout(() => { this.refresh(); }, 86400000);
+			this.refreshTimer = setTimeout(() => { this.refresh(); }, 86400000); // 设置自动刷新时间
 		}
-		else if (this.mode == 3) {
+		else if (this.mode == 3) { // “热评”
 			superagent.get('http://cmt.ithome.com/api/comment/hotcommentlist/').end((err, res) => {
 				let commentList = res.body.content.commentlist
 				for (var i in commentList) {
@@ -215,25 +213,23 @@ class contentProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
 				}
 				this.update.fire();
 			});
-			this.refreshTimer = setTimeout(() => { this.refresh(); }, 86400000);
+			this.refreshTimer = setTimeout(() => { this.refresh(); }, 86400000); // 设置自动刷新时间
 		}
 	}
-	getChildren(element?: vscode.TreeItem): vscode.TreeItem[] {
+	getChildren(element?: vscode.TreeItem): vscode.TreeItem[] { // 获取项目列表
 		if (element)
 			return [];
 		return this.list;
 	}
-	getTreeItem(element: vscode.TreeItem): vscode.TreeItem {
+	getTreeItem(element: vscode.TreeItem): vscode.TreeItem { // 获取项目
 		return element;
 	}
 }
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	refreshConfig();
-	period = <number>config.get('defaultPeriod');
-	let currentPanel: vscode.WebviewPanel | undefined = undefined;
+	period = <number>config.get('defaultPeriod'); // 仅在启动时从设置中读取
+	let panel: vscode.WebviewPanel | undefined = undefined; // 查看内容窗口
 	let account = new contentProvider(0);
 	let latest = new contentProvider(1);
 	let hot = new contentProvider(2);
@@ -243,7 +239,7 @@ export function activate(context: vscode.ExtensionContext) {
 	vscode.window.registerTreeDataProvider('hot', hot);
 	vscode.window.registerTreeDataProvider('comment', comment);
 	context.subscriptions.push(
-		vscode.commands.registerCommand('ith2ome.login', () => {
+		vscode.commands.registerCommand('ith2ome.login', () => { // 登录通行证
 			vscode.window.showInputBox({
 				ignoreFocusOut: true,
 				placeHolder: '请在此处输入您的 Cookie',
@@ -255,68 +251,68 @@ export function activate(context: vscode.ExtensionContext) {
 				});
 			})
 		}),
-		vscode.commands.registerCommand('ith2ome.logout', () => {
+		vscode.commands.registerCommand('ith2ome.logout', () => { // 退出通行证
 			userHash = '';
 			config.update('account', '', true).then(() => {
 				vscode.window.showInformationMessage('退出成功！');
 				account.refresh();
 			});
 		}),
-		vscode.commands.registerCommand('ith2ome.accountRefresh', () => {
+		vscode.commands.registerCommand('ith2ome.accountRefresh', () => { // 刷新“通行证”
 			refreshConfig();
 			account.refresh();
 		}),
-		vscode.commands.registerCommand('ith2ome.showContent', (title: string, time: string, id: number) => {
-			if (currentPanel)
-				currentPanel.reveal(vscode.window.activeTextEditor ? vscode.window.activeTextEditor.viewColumn : undefined);
+		vscode.commands.registerCommand('ith2ome.showContent', (title: string, time: string, id: number) => { // 显示新闻内容
+			if (panel)
+				panel.reveal(vscode.window.activeTextEditor ? vscode.window.activeTextEditor.viewColumn : undefined);
 			else
-				currentPanel = vscode.window.createWebviewPanel('content', 'ITH2Ome: 预览', { preserveFocus: true, viewColumn: vscode.ViewColumn.One });
+				panel = vscode.window.createWebviewPanel('content', 'ITH2Ome: 预览', { preserveFocus: true, viewColumn: vscode.ViewColumn.One });
 			superagent.get('https://api.ithome.com/json/newscontent/' + id).end((err, res) => {
-				currentPanel!.webview.html = (res.body.btheme ? '<head><style>body{filter:grayscale(100%)}</style></head>' : '') + `<h1>${title}</h1><h3>新闻源：${res.body.newssource}（${res.body.newsauthor}）｜责编：${res.body.z}</h3><h4>${time}</h4>${showImages ? res.body.detail : res.body.detail.replace(RegExp('<img.*?>', 'g'), '#图片已屏蔽#')}`;
-				if (showRelated)
+				panel!.webview.html = (res.body.btheme ? '<head><style>body{filter:grayscale(100%)}</style></head>' : '') + `<h1>${title}</h1><h3>新闻源：${res.body.newssource}（${res.body.newsauthor}）｜责编：${res.body.z}</h3><h4>${time}</h4>${showImages ? res.body.detail : res.body.detail.replace(RegExp('<img.*?>', 'g'), '#图片已屏蔽#')}`;
+				if (showRelated) // 显示相关文章
 					superagent.get('http://api.ithome.com/json/tags/0' + String(id).substring(0, 3) + `/${id}.json`).responseType('text').end((err2, res2) => {
-						currentPanel!.webview.html += `<hr><h3>相关文章</h3><ul>`;
+						panel!.webview.html += `<hr><h3>相关文章</h3><ul>`;
 						let relaList = JSON.parse(res2.body.toString().substr(16));
 						for (var i in relaList)
-							currentPanel!.webview.html += `<li><a href="${relaList[i].url}">${relaList[i].newstitle}</a></li>`;
-						currentPanel!.webview.html += '</ul>';
+							panel!.webview.html += `<li><a href="${relaList[i].url}">${relaList[i].newstitle}</a></li>`;
+						panel!.webview.html += '</ul>';
 					});
 			})
-			currentPanel.onDidDispose(() => { currentPanel = undefined; }, null, context.subscriptions);
+			panel.onDidDispose(() => { panel = undefined; }, null, context.subscriptions);
 		}),
-		vscode.commands.registerCommand('ith2ome.share', (item: ith2omeItem) => {
+		vscode.commands.registerCommand('ith2ome.share', (item: ith2omeItem) => { // 分享新闻
 			vscode.env.clipboard.writeText(item.shareInfo! + item.resourceUri).then(() => {
 				vscode.window.showInformationMessage('新闻复制成功！');
 			});
 		}),
-		vscode.commands.registerCommand('ith2ome.openBrowser', (item: vscode.TreeItem) => {
+		vscode.commands.registerCommand('ith2ome.openBrowser', (item: vscode.TreeItem) => { // 在浏览器中查看
 			vscode.commands.executeCommand('vscode.open', item.resourceUri);
 		}),
-		vscode.commands.registerCommand('ith2ome.latestRefresh', () => {
+		vscode.commands.registerCommand('ith2ome.latestRefresh', () => { // 刷新“最新”
 			refreshConfig();
 			latest.refresh();
 		}),
-		vscode.commands.registerCommand('ith2ome.hotRefresh', () => {
+		vscode.commands.registerCommand('ith2ome.hotRefresh', () => { // 刷新“热榜”
 			refreshConfig();
 			hot.refresh();
 		}),
-		vscode.commands.registerCommand('ith2ome.daily', () => {
+		vscode.commands.registerCommand('ith2ome.daily', () => { // “热榜”切换为日榜
 			period = 0;
 			hot.refresh();
 		}),
-		vscode.commands.registerCommand('ith2ome.weekly', () => {
+		vscode.commands.registerCommand('ith2ome.weekly', () => { // “热榜”切换为周榜
 			period = 1;
 			hot.refresh();
 		}),
-		vscode.commands.registerCommand('ith2ome.comment', () => {
+		vscode.commands.registerCommand('ith2ome.comment', () => { // “热榜”切换为热评
 			period = 2;
 			hot.refresh();
 		}),
-		vscode.commands.registerCommand('ith2ome.monthly', () => {
+		vscode.commands.registerCommand('ith2ome.monthly', () => { // “热榜”切换为月榜
 			period = 3;
 			hot.refresh();
 		}),
-		vscode.commands.registerCommand('ith2ome.commentRefresh', () => {
+		vscode.commands.registerCommand('ith2ome.commentRefresh', () => {  // 刷新“热评”
 			refreshConfig();
 			comment.refresh();
 		})
